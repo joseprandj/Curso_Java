4d1f7792a31b15b1a7fc301774d21a74f0a59296
JJ - Jose Junior <jose.junior@doalti.com.br>
2025-07-16 15:53:40 -0300

    Seller entity class

--- src/secao19_JDBC/ProjetoDao/application/Program.java
+++ src/secao19_JDBC/ProjetoDao/application/Program.java
@@ -1,10 +1,16 @@
 package secao19_JDBC.ProjetoDao.application;
 
 import secao19_JDBC.ProjetoDao.module.entities.Department;
+import secao19_JDBC.ProjetoDao.module.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        System.out.print(obj);
+        Department department = new Department(1, "Books");
+        System.out.println(department);
+
+        Seller seller = new Seller(1, "Teste", "teste@gmail.com", new Date(),1500.00, department);
+        System.out.println(seller);
     }
 }
